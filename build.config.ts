67d564460e88689987135e83ee2f27339b4aeb13
hgpark <hanguyl91@gmail.com>
2024-08-27 15:01:32 +0900

    chore: add missing cli build

--- build.config.ts
+++ build.config.ts
@@ -1,11 +1,26 @@
-import { defineBuildConfig } from 'unbuild'
+import { readFileSync, writeFileSync } from "node:fs";
+import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  entries: ['src/index'],
-  declaration: true,
-  failOnWarn: false,
-  clean: true,
-  rollup: {
-    emitCJS: true,
-  },
-})
+	entries: ["src/index", "src/cli"],
+	declaration: true,
+	failOnWarn: false,
+	clean: true,
+	sourcemap: true,
+	rollup: {
+		emitCJS: true,
+	},
+	hooks: {
+		"build:done": () => {
+			/**
+			 * Prepend shebang to cli entries since unbuild doesn't seem to support it
+			 */
+			const cliEntries = ["dist/cli.mjs", "dist/cli.cjs"];
+
+			for (const entry of cliEntries) {
+				const content = `#!/usr/bin/env node \n${readFileSync(entry, "utf-8")}`;
+				writeFileSync(entry, content);
+			}
+		},
+	},
+});
